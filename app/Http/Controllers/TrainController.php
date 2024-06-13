a0c3778f83841c7cb4b2fb6f7c19fe5852ea0d14
Om12ek99 <omar.elkamili@gmail.com>
2024-06-14 00:53:01 +0200

    add: added Carbon library and function index

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Train;
+use Carbon\Carbon;          //mi serve per gestire la data di oggi
 
 class TrainController extends Controller
 {
-    //
+    public function index()
+    {
+        $today = Carbon::today();
+        $trains = Train::where('orario_di_partenza', '>=', $today->startOfDay())
+                       ->where('orario_di_partenza', '<=', $today->endOfDay())
+                       ->get();
+        return view('home', compact('trains'));
+    }
 }
+
